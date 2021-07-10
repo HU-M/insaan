c7d717585aef59c984795cc528dd78903a691336
HU-M <83540224+HU-M@users.noreply.github.com>
2021-07-10 19:17:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var GuestName = document.getElementById("name1").value;
+names_of_people.push(GuestName);
+names_of_people.sort();
+
+function searching()
+{
+    var s= document.getElementById("s1").value;
+    var found=0;
+    var j;
+    for(j=0; j<names_of_people.length; j++)
+    {
+        if(s==names_of_people[j]){
+            found=found+1;
+        }
+    }
+    document.getElementById("p2").innerHTML="name found "+found+" time/s";
+    console.log("found name "+found+" time/s");
+}
+
+function sorting()
+{
+    var s= document.getElementById("s1").value;
+    var found=0;
+    var j;
+    for(j=0; j<names_of_people.length; j++)
+    {
+        if(s==names_of_people[j]){
+            found=found+1;
+        }
+    }
+    document.getElementById("p2").innerHTML="name found "+found+" time/s";
+    console.log("found name "+found+" time/s");
+}
